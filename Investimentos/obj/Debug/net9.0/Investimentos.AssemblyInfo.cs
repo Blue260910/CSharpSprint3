91dcda3d83095b56484507851d5fd8350113be65
Blue260910 <rm99667@fiap.com.br>
2025-09-01 20:10:47 -0300

    Ultimo commit sprint preparada

--- Investimentos/obj/Debug/net9.0/Investimentos.AssemblyInfo.cs
+++ Investimentos/obj/Debug/net9.0/Investimentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Investimentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+896d5cc40143e1ebfa25ec44b6e2274a6c1b7c93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6316fa6b1af6c16ad2c32e14353395f551f24379")]
 [assembly: System.Reflection.AssemblyProductAttribute("Investimentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Investimentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
